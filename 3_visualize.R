9d00d01cd8e3735a29c1f6cb4ee057d84b3436d4
Althea Archer <aaarcher@usgs.gov>
2022-07-21 12:51:42 -0500

    working pipeline to prep data for plotting viz learner

--- 3_visualize.R
+++ 3_visualize.R
@@ -1,3 +1,12 @@
-# source('3_visualize/src/')
+source('3_visualize/src/drought_learner_viz.R')
 
-p3_targets <- list()
+p3_targets <- list(
+  #### Creates a series of ggplot pngs that will get pulled together downstream
+  #
+  # NOTE: Must make sure that target 'p2_1951_2020_metadata_subset' includes the correct data
+  #
+  tar_target(p3_drought_learner_viz01_png,
+             drought_lrnr_viz01(p2_droughts_learner_viz_df,
+                                p2_streamflow_learner_viz_df),
+             format = "file")
+)
