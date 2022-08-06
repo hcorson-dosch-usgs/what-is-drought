f7769a3f63fceb432bad82201623e0c8c2d09085
Cee Nell <cnell@usgs.gov>
2022-08-06 10:27:37 -0500

    add scroll text

---
+++ src/assets/text/scrollyText.js
@@ -0,0 +1,39 @@
+export default {
+    framesContent: {
+      title: "Frames",
+      frames: [
+        {
+          id: 'a',
+          text: 'A hydrological drought means that streamflow is abnormally low.'
+        },
+        {
+          id: 'b',
+          text: 'Here, "abnormally low" is set as a threshold'
+        },
+        {
+          id: 'c',
+          text: 'Periods of severe drought happen whenever daily streamflow is below the threshold.'
+        },
+        {
+          id: 'd',
+          text: 'You might be wondering, what happens if there is an abnormally dry spring when we expect more rain?'
+        },
+        {
+          id: 'e',
+          text: 'Well, then we need to determine periods when rainfall is abnormally low for a specific week'
+        },
+        {
+          id: 'f',
+          text: 'And let our threshold change week to week'
+        },
+        {
+          id: 'g',
+          text: 'Now we capture droughts when streamflow is abnormally low for a given week.'
+        },
+        {
+          id: 'h',
+          text: 'This variable threshold changes the timing and number of droughts over the year.'
+        }
+        ]
+    },
+  };
