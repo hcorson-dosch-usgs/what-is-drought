08ab02c056b3700b17be7ceed8a6021ccd780d6b
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-04-14 14:53:52 -0500

    add draft contribution statements

--- src/assets/text/authors.js
+++ src/assets/text/authors.js
@@ -9,7 +9,7 @@ export default {
         initials: 'AA1',
         profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
         role: 'lead developer',
-        contribution: 'led the design and development of the Vue website, drew the illustrations, and designed the charts'
+        contribution: 'led the design and development of the Vue website, drew the illustrations, and made the charts'
       }
     ],
     // do not delete section. delete any or all individuals as needed. modify content as needed
@@ -22,7 +22,7 @@ export default {
         initials: 'HCD',
         profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
         role: 'contributor',
-        contribution: 'built the initial pipeline and reviewed code'
+        contribution: 'contributed to website development and reviewed code'
       },
       {
         firstName: 'Cee',
@@ -31,7 +31,7 @@ export default {
         initials: 'CN',
         profile_link: 'https://www.usgs.gov/staff-profiles/cee-nell',
         role: 'supervisor',
-        contribution: 'acted in an avisory role and reviewed code'
+        contribution: 'acted in an advisory role, contributed to website development, and reviewed code'
       }
       ]
 };
