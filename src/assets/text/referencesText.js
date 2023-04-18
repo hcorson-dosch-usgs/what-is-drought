ec0434ba75a3a97efd2c6a796ccda503ae3cd4f5
Althea A Archer <aaarcher@usgs.gov>
2023-04-18 13:18:37 -0500

    added reference to drought prediction page

--- src/assets/text/referencesText.js
+++ src/assets/text/referencesText.js
@@ -4,8 +4,17 @@ export default {
       references: [
         {
           reference:
-            "Streamflow Drought Metrics for Select United States Geological Survey Streamgages for Three Different Time Periods from 1921 - 2020.",
+            "Drought Prediction Science",
             num:"1",
+            authors: "Smith, E. (2023)",
+            title: "Drought Prediction Science.",
+            link:"https://www.usgs.gov/mission-areas/water-resources/science/drought-prediction-science",
+            ref:" https://www.usgs.gov/mission-areas/water-resources/science/drought-prediction-science"
+        },
+        {
+          reference:
+            "Streamflow Drought Metrics for Select United States Geological Survey Streamgages for Three Different Time Periods from 1921 - 2020.",
+            num:"2",
             authors: "Simeone, C. (2022)",
             title: "Streamflow Drought Metrics for Select United States Geological Survey Streamgages for Three Different Time Periods from 1921 - 2020.",
             link:"https://doi.org/10.5066/P92FAASD",
@@ -14,7 +23,7 @@ export default {
         {
           reference:
             "Hammond Going Below Low Flows",
-            num:"2",
+            num:"3",
             authors: "Hammond, J. C., Simeone, C., Hecht, J. S., Hodgkins, G. A., Lombard, M., McCabe, G., et al. (2022)",
             title: "Going beyond low flows: Streamflow drought deficit and duration illuminate distinct spatiotemporal drought patterns and trends in the U.S. during the last century.",
             link:"https://doi.org/10.1029/2022WR031930",
@@ -23,7 +32,7 @@ export default {
         {
           reference:
             "Van Loon, A.F. (2015), Hydrological drought explained. WIREs Water, 2: 359-392. https://doi.org/10.1002/wat2.1085",
-            num:"3",
+            num:"4",
             authors: "Van Loon, A. F. (2015)",
             title: "Hydrological drought explained.",
             link:"https://doi.org/10.1002/wat2.1085",
