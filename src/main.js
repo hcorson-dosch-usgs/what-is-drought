88c30bd7fda0ade9c6a2a16218d7411e786814ec
Cee <cnell@usgs.gov>
2025-03-31 22:43:58 -0500

    find missing icons

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 // social icons
 import { faSquareXTwitter, faFacebookSquare, faGithub, faFlickr, faYoutubeSquare, faInstagram } from "@fortawesome/free-brands-svg-icons";
 library.add(faSquareXTwitter, faFacebookSquare, faGithub, faFlickr, faYoutubeSquare, faInstagram);
+library.add(faArrowRight, faArrowLeft)
 
 import App from './App.vue'
 import router from './router'
